b8c2129da97603740c8f56a034edcd4a4f33e5d9
jcdenton <ivan.bruzgin@gmail.com>
2013-10-12 00:19:17 +0300

    Fixed indents.

--- python/problem004.py
+++ python/problem004.py
@@ -6,4 +6,6 @@ def is_palyndrome(n):
     return str(n) == str(n)[::-1]
 
 
-print reduce(max, ifilter(is_palyndrome, starmap(mul, product(xrange(100, 1000), repeat=2))))
+print reduce(max, ifilter(
+    is_palyndrome,
+    starmap(mul, product(xrange(100, 1000), repeat=2))))
